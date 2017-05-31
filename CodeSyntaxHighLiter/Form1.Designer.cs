39e2bf5990894ee4350a011408dfa5df2b4622a4
Khan <Abdukhan@ES.AD.ADP.com>
2017-05-31 11:53:34 +0530

    adding files.

---
+++ CodeSyntaxHighLiter/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace CodeSyntaxHighLiter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ResultTextBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.CodeTextBox = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tvw_display = new System.Windows.Forms.TreeView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ResultTextBox
+            // 
+            this.ResultTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ResultTextBox.Font = new System.Drawing.Font("Book Antiqua", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResultTextBox.Location = new System.Drawing.Point(527, 56);
+            this.ResultTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.ResultTextBox.Name = "ResultTextBox";
+            this.ResultTextBox.Size = new System.Drawing.Size(412, 391);
+            this.ResultTextBox.TabIndex = 1;
+            this.ResultTextBox.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(431, 293);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Show HTML";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(15, 475);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(23, 25);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(412, 321);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Actual Code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label2.Location = new System.Drawing.Point(521, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Result HTML for Code ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label3.Location = new System.Drawing.Point(21, 456);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(183, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Beatified Look on Browser/IDE";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(431, 145);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(87, 24);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bell MT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 19);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            this.label4.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Silver;
+            this.button3.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(795, 20);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(141, 28);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Copy To Clipboard";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label5.Location = new System.Drawing.Point(431, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Code Language";
+            this.label5.Visible = false;
+            // 
+            // CodeTextBox
+            // 
+            this.CodeTextBox.Location = new System.Drawing.Point(15, 56);
+            this.CodeTextBox.Name = "CodeTextBox";
+            this.CodeTextBox.Size = new System.Drawing.Size(412, 390);
+            this.CodeTextBox.TabIndex = 13;
+            this.CodeTextBox.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(210, 456);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 16);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "label6";
+            this.label6.Visible = false;
+            // 
+            // tvw_display
+            // 
+            this.tvw_display.Location = new System.Drawing.Point(527, 475);
+            this.tvw_display.Name = "tvw_display";
+            this.tvw_display.Size = new System.Drawing.Size(409, 321);
+            this.tvw_display.TabIndex = 15;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Silver;
+            this.button2.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(432, 390);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 28);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Show HTML";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(951, 809);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tvw_display);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.CodeTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ResultTextBox);
+            this.Font = new System.Drawing.Font("Book Antiqua", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Red;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Abdul : Code Syntax Highlighter";
+            this.TransparencyKey = System.Drawing.Color.DarkSalmon;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox ResultTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox CodeTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TreeView tvw_display;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ CodeSyntaxHighLiter/Form1.cs
@@ -0,0 +1,208 @@
+﻿using ColorCode;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodeSyntaxHighLiter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.AddRange(Languages.All.Select(a => a.Id).ToArray());
+            label4.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string code = CodeTextBox.Text;//File.ReadAllText(Server.MapPath("~/Test.txt"));
+                if (!string.IsNullOrEmpty(code))
+                {
+
+                    if (comboBox1.SelectedItem != null && !string.IsNullOrEmpty(comboBox1.SelectedItem.ToString()))
+                    {
+
+
+                        string finalCode = new CodeColorizer().Colorize(code, Languages.FindById(comboBox1.SelectedItem.ToString()));
+                        ResultTextBox.Text = finalCode;
+                        DisplayHtml(finalCode); label4.Text = "Hurray!!...I Am Genious.Don't Say Thanks...My Pleasure.";
+                        label4.Visible = true;
+                        label6.Text = Languages.FindById(comboBox1.SelectedItem.ToString()).Name;
+                        label6.Visible = true;
+                    }
+
+                    else
+                    {
+                        label6.Visible = false;
+                        label4.Visible = true;
+                        label4.Text = "Dammit!!!...I Am Mad,Please Select Language ";
+                    }
+                }
+                else
+                {
+                    label6.Visible = false;
+                    label4.Visible = true;
+                    label4.Text = "Hungry!!!...Please Feed Some Actual Code Text.";
+                }
+            }
+            catch (Exception)
+            {
+                label4.Visible = true;
+                label4.Text = "Help!!!...I Need To Be Hospitalized...Try Again Or Contact Abdul.";
+            }
+
+        }
+
+
+        private void DisplayHtml(string html)
+        {
+            webBrowser1.Navigate("about:blank");
+            if (webBrowser1.Document != null)
+            {
+                webBrowser1.Document.Write(string.Empty);
+            }
+            webBrowser1.DocumentText = html;
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            if (!string.IsNullOrEmpty(ResultTextBox.Text))
+            {
+                Clipboard.SetText(ResultTextBox.Text);
+            }
+            else
+            {
+                label6.Visible = false;
+                label4.Visible = true;
+                label4.Text = "Crap!!! Nothing To Copy..";
+            }
+        }
+        public class JsonTag
+        {
+            public JsonTag(JsonReader reader)
+            {
+                TokenType = reader.TokenType;
+                Value = reader.Value;
+                ValueType = reader.ValueType;
+            }
+
+            public JsonToken TokenType { get; set; }
+            public object Value { get; set; }
+            public Type ValueType { get; set; }
+        }
+        private TreeNode Json2Tree(JObject obj)
+        {
+            //create the parent node
+            TreeNode parent = new TreeNode();
+            //loop through the obj. all token should be pair<key, value>
+            foreach (var token in obj)
+            {
+                //change the display Content of the parent
+                parent.Text = token.Key.ToString();
+                //create the child node
+                TreeNode child = new TreeNode();
+                child.Text = token.Key.ToString();
+                //check if the value is of type obj recall the method
+                if (token.Value.Type.ToString() == "Object")
+                {
+                    // child.Text = token.Key.ToString();
+                    //create a new JObject using the the Token.value
+                    JObject o = (JObject)token.Value;
+                    //recall the method
+                    child = Json2Tree(o);
+                    //add the child to the parentNode
+                    parent.Nodes.Add(child);
+                }
+                //if type is of array
+                else if (token.Value.Type.ToString() == "Array")
+                {
+                    int ix = -1;
+                    //  child.Text = token.Key.ToString();
+                    //loop though the array
+                    foreach (var itm in token.Value)
+                    {
+                        //check if value is an Array of objects
+                        if (itm.Type.ToString() == "Object")
+                        {
+                            TreeNode objTN = new TreeNode();
+                            //child.Text = token.Key.ToString();
+                            //call back the method
+                            ix++;
+
+                            JObject o = (JObject)itm;
+                            objTN = Json2Tree(o);
+                            child.Text = token.Key.ToString();
+                            objTN.Text = token.Key.ToString() + "[" + ix + "]";
+                            child.Nodes.Add(objTN);
+                            
+                            //parent.Nodes.Add(child);
+                        }
+                        //regular array string, int, etc
+                        else if (itm.Type.ToString() == "Array")
+                        {
+                            ix++;
+                            TreeNode dataArray = new TreeNode();
+                            foreach (var data in itm)
+                            {
+                                dataArray.Text = token.Key.ToString() + "[" + ix + "]";
+                                dataArray.Nodes.Add(data.ToString());
+                            }
+                            child.Nodes.Add(dataArray);
+                        }
+
+                        else
+                        {
+                            child.Nodes.Add(itm.ToString());
+                        }
+                    }
+                    parent.Nodes.Add(child);
+                }
+                else
+                {
+                    //if token.Value is not nested
+                    // child.Text = token.Key.ToString();
+                    //change the value into N/A if value == null or an empty string 
+                    if (token.Value.ToString() == "")
+                        child.Nodes.Add("N/A");
+                    else
+                        child.Nodes.Add(token.Value.ToString());
+                    parent.Nodes.Add(child);
+                }
+            }
+            return parent;
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string json = CodeTextBox.Text;
+                JObject obj = JObject.Parse(json);
+                tvw_display.Nodes.Clear();
+                TreeNode parent = Json2Tree(obj);
+                parent.Text = "Root Object";
+                tvw_display.Nodes.Add(parent);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR");
+            }
+        }
+
+    }
+}
